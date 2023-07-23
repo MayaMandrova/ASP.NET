ff172ba4d0fa2f1d9c0e86e2ee490ed56ca39894
MayaMandrova <maya.mandrova@gmail.com>
2023-07-24 01:25:20 +0200

    Modified HomeController, create ProductModel, _Layout, copywriting

--- Lesson_5/Controllers/HomeController.cs
+++ Lesson_5/Controllers/HomeController.cs
@@ -7,6 +7,17 @@ namespace Lesson_5.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    
+    public string[] name = { "Книга", "Полнометражный фильм", "Сериал", "Комикс", "Короткометражный фильм", "Мультфильм", "Журнал" };
+
+    public string[] description = { "Ведьмак", "Игра престолов", "Волшебник Земноморья", "Стрелок", "Лабиринты Ехо", "Меч короля Артура", "Эдвар руки-ножницы", 
+        "Варкрафт", "Звездная пыль", "Другой мир", "Морбиус", "Всё везде и сразу", "Чёрный Адам", "Страна снов", "Фантастические твари", "Три тысячи лет желаний",
+        "Тролль", "Пиноккио", "Сердце Пармы", "Этерна"
+    };
+   
+    public double[] priceForPiece = { 1200, 500, 900, 450, 1160, 460, 444, 512, 777, 994, 1031, 846, 117, 462, 654, 951, 753, 357, 159, 852 };
+    
+    public int[] quantity = { 51, 92, 4, 6, 74, 88, 64, 888, 210, 99, 61, 75, 111, 642, 132, 845, 67, 52, 77, 100 };
 
     public HomeController(ILogger<HomeController> logger)
     {
@@ -15,17 +26,47 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        var model = CreateProductModels();
+        return View(model);
     }
 
+    public IActionResult NewTabForChecking()
+    {
+        return View();
+    }
+    
     public IActionResult Privacy()
     {
         return View();
     }
-
+    
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    private List<ProductModel> CreateProductModels()
+    {
+        List<ProductModel> productModels = new List<ProductModel>();
+        Random randomName = new Random();
+        Random randomDescription = new Random();
+        Random randomPriceForPiece = new Random(); 
+        Random randomQuantity = new Random();
+
+        for (int i = 0; i < 20; i++)
+        {
+            productModels.Add(new ProductModel()
+            {
+                Id = i + 1,
+                Name = name[randomName.Next(name.Length)],
+                Description = description[randomDescription.Next(description.Length)],
+                PriceForPiece = priceForPiece[randomPriceForPiece.Next(priceForPiece.Length)],
+                Quantity = quantity[randomQuantity.Next(quantity.Length)],
+                Sign = "✎⌫"
+            });
+        }
+
+        return productModels;
     }
 }
---
+++ Lesson_5/Models/ProductModel.cs
@@ -0,0 +1,11 @@
+namespace Lesson_5.Models;
+
+public class ProductModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double PriceForPiece { get; set; }
+    public int Quantity { get; set; }
+    public string Sign { get; set; }
+}
